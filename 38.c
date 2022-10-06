ae10a96c8c5af8053f4fb15ad161a32e00ed04d3
Aman singh <113423671+CodeWithAmann@users.noreply.github.com>
2022-10-07 01:08:14 +0530

    Update 38.c

--- 38.c
+++ 38.c
@@ -1,22 +1,22 @@
-// #include <stdio.h>
-// int main()
-// {
-//     int a, d;
-//     printf("Enter the number");
-//     scanf("%d", &a);
-//     while (a > 0)
-//     {
-//         d = a % 10;
-//         if (d != 0 && d != 1)
-//         {
-//             printf("Enter number is not binary ");
-//             return 0;
-//         }
-//         a /= 10;
-//     }
-//     if (a == 0)
-//     {
-//         printf("Enter number is binary ");
-//     }
-//     return 0;
-// }
+ #include <stdio.h>
+ int main()
+ {
+     int a, d;
+     printf("Enter the number");
+     scanf("%d", &a);
+     while (a > 0)
+     {
+         d = a % 10;
+         if (d != 0 && d != 1)
+         {
+             printf("Enter number is not binary ");
+             return 0;
+         }
+         a /= 10;
+     }
+     if (a == 0)
+     {
+         printf("Enter number is binary ");
+     }
+     return 0;
+ }
